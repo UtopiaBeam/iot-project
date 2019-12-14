e305fe4a20d8a1f766e6bb634c6649f21719f1d5
UtopiaBeam <utopiabeam@gmail.com>
2019-12-14 14:59:02 +0700

    :sparkles: Add logout button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>This is beautiful homepage</div>
+  <div class="w-full h-full flex flex-col justify-center items-center">
+    <button
+      class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      @click="logout"
+    >Logout</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private database = firebase.database();
+  private async logout() {
+    await firebase.auth().signOut();
+    this.$router.replace('login');
+  }
+}
 </script>
