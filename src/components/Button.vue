80f14d1ef9420ae28b1a61d5e5a9bfc8b7508931
UtopiaBeam <utopiabeam@gmail.com>
2019-12-14 16:53:54 +0700

    :sparkles: Add Button

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    v-if="isLocked"
+    class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+    @click="toggle"
+  >Unlock</button>
+  <button
+    v-else
+    class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+    @click="toggle"
+  >Lock</button>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Button extends Vue {
+  @Prop(String) private id!: string;
+  @Prop(Boolean) private isLocked!: boolean;
+  @Prop(Number) private index!: number;
+
+  private toggle() {
+    this.isLocked = !this.isLocked;
+    this.$emit('toggled', this.id, this.isLocked, this.index);
+  }
+}
+</script>
